6baeb2e8e36caf399f6f987012876b421ae37444
Keval-sprybit <keval@sprybit.com>
2023-03-06 18:36:11 +0530

    fix animation

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -5,7 +5,9 @@
   
   <router-link :to="{ name: 'byName', params: { name: pokemon.name  } }">
     <h3 class="font-bold">{{ pokemonInfo.id }} {{ pokemon.name }}</h3>
-    <img class="hover:filter-none hover:origin-top-left hover:saturate-200  object-cover" :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/'+pokemonInfo.id +'.svg'">
+    <!-- <img class="hover:filter-none hover:origin-top-left hover:saturate-200  object-cover" :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/'+pokemonInfo.id +'.svg'"> -->
+
+    <img class="object-cover" :src="'https://www.pkparaiso.com/imagenes/xy/sprites/animados/'+pokemon.name +'.gif'">
   </router-link>
 
   
